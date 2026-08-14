18268f21d94d8c7f5884dbebe09be790be915eea
Yashdarji31 <darji2730@gmail.com>
2026-08-15 00:13:47 +0530

    feat: add billing types

---
+++ frontend/src/types/billing/index.ts
@@ -0,0 +1,40 @@
+export interface Billing {
+  id: number;
+
+  appointment_id: number;
+  doctor_id: number;
+  patient_id: number;
+
+  amount: number;
+
+  payment_status: string;
+  payment_method: string;
+
+  description: string;
+
+  created_at: string;
+}
+
+
+export interface BillingCreate {
+  appointment_id: number;
+
+  amount: number;
+
+  payment_status: string;
+
+  payment_method: string;
+
+  description: string;
+}
+
+
+export interface BillingUpdate {
+  amount?: number;
+
+  payment_status?: string;
+
+  payment_method?: string;
+
+  description?: string;
+}
